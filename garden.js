4ece41ec2c123979fd9177eba9294329fa7a909d
Veronica Ruiz <veronica.ruizab@gmail.com>
2023-09-21 17:17:11 +0200

    changes to garden

--- garden.js
+++ garden.js
@@ -3,32 +3,35 @@ const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
 for (let i = 1; i <= 151; i++) {
-    fetch(URL + i)
-        .then((response) => { console.log(response); return response.json() })
-        .then(data => {
-            const tipos = data.types.map(type => type.type.name);
-            if (tipos.includes("grass")) {
-                mostrarPokemon(data);
-            }
-        })
+  fetch(URL + i)
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      const tipos = data.types.map((type) => type.type.name);
+      if (tipos.includes("grass")) {
+        mostrarPokemon(data);
+      }
+    });
 }
 
 function mostrarPokemon(poke) {
+  let tipos = poke.types.map(
+    (type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`
+  );
+  tipos = tipos.join("");
 
-    let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
-    tipos = tipos.join('');
+  let pokeId = poke.id.toString();
+  if (pokeId.length === 1) {
+    pokeId = "00" + pokeId;
+  } else if (pokeId.length === 2) {
+    pokeId = "0" + pokeId;
+  }
 
-    let pokeId = poke.id.toString();
-    if (pokeId.length === 1) {
-        pokeId = "00" + pokeId;
-    } else if (pokeId.length === 2) {
-        pokeId = "0" + pokeId;
-    }
-
-
-    const div = document.createElement("div");
-    div.classList.add("pokemon");
-    div.innerHTML = `
+  const div = document.createElement("div");
+  div.classList.add("pokemon");
+  div.innerHTML = `
         <p class="pokemon-id-back">#${pokeId}</p>
         <div class="pokemon-imagen">
             <img src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}">
@@ -47,28 +50,28 @@ function mostrarPokemon(poke) {
             </div>
         </div>
     `;
-    listaPokemon.append(div);
+  listaPokemon.append(div);
 }
 
-botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
+botonesHeader.forEach((boton) =>
+  boton.addEventListener("click", (event) => {
     const botonId = event.currentTarget.id;
 
     listaPokemon.innerHTML = "";
 
     for (let i = 1; i <= 151; i++) {
-        fetch(URL + i)
-            .then((response) => response.json())
-            .then(data => {
-
-                if (botonId === "ver-todos") {
-                    mostrarPokemon(data);
-                } else {
-                    const tipos = data.types.map(type => type.type.name);
-                    if (tipos.some(tipo => tipo.includes(botonId))) {
-                        mostrarPokemon(data);
-                    }
-                }
-
-            })
+      fetch(URL + i)
+        .then((response) => response.json())
+        .then((data) => {
+          if (botonId === "ver-todos") {
+            mostrarPokemon(data);
+          } else {
+            const tipos = data.types.map((type) => type.type.name);
+            if (tipos.some((tipo) => tipo.includes(botonId))) {
+              mostrarPokemon(data);
+            }
+          }
+        });
     }
-}))
+  })
+);
